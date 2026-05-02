5f0c860d49dd8b203367317e870230081444f16f
HBPencil28 <u24868346@tuks.co.za>
2026-05-03 01:12:57 +0200

    Basic BaseInput code.

--- frontend/components/ui/BaseInput.vue
+++ frontend/components/ui/BaseInput.vue
@@ -1,3 +1,12 @@
-<template> 
-
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
 </template>
+
+<script setup>
+    defineProps({
+    modelValue: String
+    })
+</script>
