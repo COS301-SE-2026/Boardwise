e5aafa13c0055e7611a382f39248f41afff52380
Njabs-codez <njabsmathonsi52@gmail.com>
2026-05-03 23:44:08 +0200

    Created some necessary models and DTOs

--- backend/src/main/java/com/boardwise/backend/user_service/controllers/Authcontrollers.java
+++ backend/src/main/java/com/boardwise/backend/user_service/controllers/Authcontrollers.java
@@ -1,28 +1,35 @@
 package com.boardwise.backend.user_service.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boardwise.backend.user_service.dtos.AuthResponseDTO;
+import com.boardwise.backend.user_service.services.AuthService;
+
 @RestController
 @RequestMapping("/api/auth/")
 public class Authcontrollers {
 
-    
+    @Autowired
+    private AuthService service;
 
     @PostMapping("/register")
-    public void register(){
-
+    public ResponseEntity<AuthResponseDTO> register(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    public void login(){
-
+    public ResponseEntity<AuthResponseDTO> login(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/logout")
-    public void logout(){
-
+    public ResponseEntity<AuthResponseDTO> logout(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/boardwise/backend/user_service/models/User.java
+++ backend/src/main/java/com/boardwise/backend/user_service/models/User.java
@@ -1,11 +1,38 @@
 package com.boardwise.backend.user_service.models;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Document(collection = "Users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
     
     @Id
-    private Integer id;
+    private String id;
+    private String username;
+    private String emailAddress;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String bio;
+    private String profilePic; // image url from cloud buckets
+    private Preferences preferences;
+    private LocalDateTime createdAt;
+
+    @DocumentReference
+    private Boardgame ownedGames;
+
 }
