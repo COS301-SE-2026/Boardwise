62ddc9a05fc7930856e72b644e00801fb8c44dd6
Njabs-codez <njabsmathonsi52@gmail.com>
2026-05-04 15:25:50 +0200

    My implementation for the UserDetails interface

---
+++ backend/src/main/java/com/boardwise/backend/user_service/models/UserDetailImpl.java
@@ -0,0 +1,34 @@
+package com.boardwise.backend.user_service.models;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailImpl(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+}
