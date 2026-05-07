1acb736cd8cd3b73ee43d9008f50dd6cd6d23cf4
bandilem-git <u24675394@tuks.co.za>
2026-05-07 20:05:33 +0200

    chore: removed unimplemented BoardGame to use user_services boardgame

--- backend/src/main/java/com/boardwise/backend/marketplace/controller/MarketplaceControllers.java
+++ backend/src/main/java/com/boardwise/backend/marketplace/controller/MarketplaceControllers.java
@@ -7,27 +7,37 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Component
 @RestController
+@RequestMapping("/api/marketplace/")
+
 public class MarketplaceControllers {
+    // loading env application-properties: spring.mongodb.uri=${DB_URL}
+    @Value("${spring.mongodb.uri}")
+    private String db_url;
 
     // AC-MKT-01: Get All Active Listings
-    @GetMapping("/api/marketplace/listings")
+    @GetMapping("listings")
     public String getAllListings(@RequestParam String param) {
         // TODO: process GET request
-
+        System.out.println(db_url);
         return null;
     }
 
     // AC-MKT-02: Get Listing by ID
-    @GetMapping("/api/marketplace/listings/{listingId}") // to be changed
+    @GetMapping("listings/{listingId}") // to be changed
     public String getListingById(@RequestParam String param) {
         // TODO: process GET request
         return null;
     }
 
     // AC-MKT-03: Create a Listing
-    @PostMapping("/api/marketplace/listings")
+    @PostMapping("listings")
     public String createListing(@RequestBody String entity) {
         // TODO: process POST request
 
@@ -35,7 +45,7 @@ public class MarketplaceControllers {
     }
 
     // AC-MKT-04: Update a Listing
-    @PatchMapping("/api/marketplace/listings/{listingId}")
+    @PatchMapping("listings/{listingId}")
     public String updateListing(@RequestBody String entity) {
         // TODO: process PATCH request
 
@@ -43,7 +53,7 @@ public class MarketplaceControllers {
     }
 
     // AC-MKT-05: Delete a Listing
-    @DeleteMapping("/api/marketplace/listings/{listingId}")
+    @DeleteMapping("listings/{listingId}")
     public String deleteListing(@RequestBody String entity) {
         // TODO: process DELETE request
 
@@ -51,11 +61,12 @@ public class MarketplaceControllers {
     }
 
     // AC-MKT-06: Get Authenticated User's Listings
-    @GetMapping("/api/marketplace/listings/{listingId}") // to be changed
+    @GetMapping("listings/user") // to be changed
     public String getUserListings(@RequestParam String param) {
         // TODO: process GET request
         return null;
     }
 
-    // TODO: AC-MKT-07: Get Retail Sources for a Game Title 
+    // TODO: AC-MKT-07: Get Retail Sources for a Game Title
+
 }
--- backend/src/main/java/com/boardwise/backend/marketplace/model/BoardGame.java
+++
@@ -1,5 +0,0 @@
-package com.boardwise.backend.marketplace.model;
-
-public class BoardGame {
-
-}
