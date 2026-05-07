d0e5895c401a99e715adfbb6f553e73f482d5039
bandilem-git <u24675394@tuks.co.za>
2026-05-07 20:11:42 +0200

    fix:  imported lombok.Data

--- backend/src/main/java/com/boardwise/backend/marketplace/model/Listing.java
+++ backend/src/main/java/com/boardwise/backend/marketplace/model/Listing.java
@@ -8,11 +8,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data
 @Document(collection = "Listings")
 public class Listing {
     @Id
