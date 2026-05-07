4c9c3381f44fe790d681c935eeda9051f1167ebd
W-Wolf-K <175476101+W-Wolf-K@users.noreply.github.com>
2026-05-07 15:20:06 +0200

    chore: Added config for websockets and security, and added necessary jwt utility and filter

--- backend/src/main/java/com/boardwise/backend/shared/config/SecurityConfig.java
+++ backend/src/main/java/com/boardwise/backend/shared/config/SecurityConfig.java
@@ -1,5 +1,36 @@
 package com.boardwise.backend.shared.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.boardwise.backend.shared.security.JwtFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-    
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/health").permitAll()
+                .requestMatchers("/ws/vault/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+            
+            return http.build();
+    }
+
 }
