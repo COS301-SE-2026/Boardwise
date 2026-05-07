757f0b630bfb09e662f35dc267f2115b172f606f
bandilem-git <u24675394@tuks.co.za>
2026-05-07 19:58:35 +0200

    refactor: added lombok imports

--- backend/src/main/java/com/boardwise/backend/marketplace/model/Listing.java
+++ backend/src/main/java/com/boardwise/backend/marketplace/model/Listing.java
@@ -7,6 +7,12 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Document(collection = "Listings")
 public class Listing {
     @Id
