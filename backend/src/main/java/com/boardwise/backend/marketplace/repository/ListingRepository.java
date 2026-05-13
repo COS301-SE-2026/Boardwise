d994a024dd225201d6cb559c8d7c3ad82ec843c3
bandilem-git <u24675394@tuks.co.za>
2026-05-13 10:40:34 +0200

    chore: repositories implemented for Order and Listing

---
+++ backend/src/main/java/com/boardwise/backend/marketplace/repository/ListingRepository.java
@@ -0,0 +1,22 @@
+package com.boardwise.backend.marketplace.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.boardwise.backend.marketplace.model.Listing;
+import java.util.List;
+import com.boardwise.backend.marketplace.enums.ItemType;
+import com.boardwise.backend.marketplace.enums.ListingStatus;
+import com.boardwise.backend.marketplace.enums.ListingType;
+
+public interface ListingRepository extends MongoRepository<Listing, String> {
+    List<Listing> findByUserId(String userId);
+
+    List<Listing> findByGameId(String gameId);
+
+    List<Listing> findByItemType(ItemType itemType);
+
+    List<Listing> findByStatus(ListingStatus status);
+
+    List<Listing> findByListingType(ListingType listingType);
+
+}
