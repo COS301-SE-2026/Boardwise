f0503b928993823bd2eb0fd4ae264134b9acf8aa
HBPencil28 <u24868346@tuks.co.za>
2026-05-14 15:59:25 +0200

    Frontend: New shared component and added new components for the other pages.

---
+++ frontend/components/layout/Navbar.vue
@@ -0,0 +1,57 @@
+<template> 
+    <nav class="navbar">
+
+        <NuxtLink to="/" class="logo">
+            Boardwise
+        </NuxtLink>
+
+        <div class="links">
+            
+            <NuxtLink to="/marketplace">
+                marketplace
+            </NuxtLink>
+
+            <NuxtLink to="/library">
+                Library
+            </NuxtLink>
+
+            <NuxtLink to="/profile">
+                Profile
+            </NuxtLink>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.navbar {
+    height: 70px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 0 24px;
+
+    border-bottom: 1px solid #eee;
+    background: white;
+}
+
+.logo {
+    font-size: 20px;
+    font-weight: bold;
+
+    text-decoration: none;
+
+    color: #6C3BFF;
+}
+
+.links {
+    display: flex;
+    gap: 20px;
+}
+
+.links a {
+    text-decoration: none;
+    color: #333;
+}
+</style>
