57d3777bed02bd411d96809293e5ea7ab0c57bbf
bandilem-git <u24675394@tuks.co.za>
2026-05-16 17:04:11 +0200

    feat: Listing service Testing

---
+++ backend/src/test/java/com/boardwise/backend/marketplace/ListingServiceTest.java
@@ -0,0 +1,32 @@
+package com.boardwise.backend.marketplace;
+
+import org.junit.jupiter.api.Test;
+import com.boardwise.backend.marketplace.service.*;
+
+public class ListingServiceTest {
+    private String test_username = "testUsername";
+    private String test_game_id = "testGameId";
+    private String test_item_type_boardgame = "boardgame";
+    private String test_item_type_asset = "asset";
+    private double test_price = 299.99;
+    private String test_description = "test description";
+    private String test_imageUrl = "http://example.com/image.jpg";
+    private String[] test_genres = { "negotiation" };
+
+    // create listing test
+    @Test
+    void create_SALE_Listing_valid_details() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+    }
+
+    @Test
+    void create_RENTAL_Listing_valid_details() {
+
+    }
+    // delete listing test
+    
+}
