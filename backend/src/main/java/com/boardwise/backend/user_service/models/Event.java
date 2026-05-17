2647fff9523657d0924649bd499e128bed680fb8
Njabs-codez <njabsmathonsi52@gmail.com>
2026-05-17 15:56:50 +0200

    Added fields to Event model.

--- backend/src/main/java/com/boardwise/backend/user_service/models/Event.java
+++ backend/src/main/java/com/boardwise/backend/user_service/models/Event.java
@@ -1,7 +1,12 @@
 package com.boardwise.backend.user_service.models;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
 
@@ -11,5 +16,26 @@ public class Event {
     
     @Id
     private String id;
+
+    private String name;
+
+    private String description;
+
+    private Date date;
+
+    private String time;
+
+    private String location;
+
+    private String visibility;
+
+    @Field("creator_id")
+    private String creatorId;
+
+    private List<String> games; // store the games that'll be played
+
+    @Field("created_at")
+    private Instant createdAt;
+
     
 }
