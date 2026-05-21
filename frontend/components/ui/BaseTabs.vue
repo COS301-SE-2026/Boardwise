628a3330e351eee26733ba96e3fc3bac092cf34c
Njabs-codez <njabsmathonsi52@gmail.com>
2026-05-21 02:45:03 +0200

    Refactored to use v-tabs and v-tab

--- frontend/components/ui/BaseTabs.vue
+++ frontend/components/ui/BaseTabs.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="tabs">
-        <div 
+    <v-tabs
+        :model-value="activeTab"
+        @update:model-value="$emit('change', $event)"
+    >
+        <v-tab
             v-for="tab in tabs"
             :key="tab"
-            class="tab"
-            :class="{ active: tab === activeTab }"
-            @click="$emit('change', tab)"
+            :value="tab"
         >
             {{ tab }}
-        </div>
-    </div>
+        </v-tab>    
+    </v-tabs>
 </template>
 
 <script setup>
@@ -20,41 +21,3 @@ defineProps({
 
 defineEmits(['change'])
 </script>
-
-<style scoped> 
-.tabs {
-    display: flex;
-    gap: 32px;
-    border-bottom: 1px solid #E5E7EB;
-    margin-top: 32px;
-}
-
-.tab {
-    position: relative;
-    padding-bottom: 14px;
-    cursor: pointer;
-    font-weight: 600;
-    color: #777;
-    transition: 0.2s ease;
-}
-
-.tab:hover {
-    color: #111;
-}
-
-.active {
-    background: #6C3BFF;
-    color: white;
-}
-
-.active::after {
-    content: '';
-    position: absolute;
-    left: 0;
-    bottom: -1px;
-    width: 100%;
-    height: 3px;
-    background: #6C3BFF;
-    border-radius: 999px;
-}
-</style>
