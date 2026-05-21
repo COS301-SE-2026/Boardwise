36295dcb1bc7acfb086f87fe62cadde47014face
HBPencil28 <u24868346@tuks.co.za>
2026-05-21 02:04:45 +0200

    Base Component: Adjusted the BaseGrid to vuetify component

--- frontend/components/ui/BaseGrid.vue
+++ frontend/components/ui/BaseGrid.vue
@@ -1,14 +1,26 @@
 <template> 
-    <div class="grid">
+    <div class="base-grid">
         <slot />
     </div>
 </template>
 
+<script setup>
+const props = defineProps({
+    cols: {
+        type: String,
+        default: '280px'
+    },
+    gap: {
+        type: String,
+        default: '20px'
+    }
+})
+</script>
+
 <style scoped> 
-.grid {
-    display: flex;
-    grid-template-columns: 
-        repeat(auto-fit, minmax(220, 1fr));
-    gap: 24px;
+.base-grid {
+    display: grid;
+    grid-template-columns: v-bind("'repeat(auto-fit, minmax(' + cols + ', 1fr))'");
+    gap: v-bind(gap);
 }
 </style>
