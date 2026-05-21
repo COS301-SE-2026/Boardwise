0b3b2b6feecbc45510c0cc170f819616e3a75414
HBPencil28 <u24868346@tuks.co.za>
2026-05-21 03:32:52 +0200

    Specialised Component: Made the Logout button using vuetify component

---
+++ frontend/components/features/auth/LogOutButton.vue
@@ -0,0 +1,21 @@
+<template> 
+    <BaseButton variant="secondary" @click="handleLogout">
+        <v-icon start>
+            mdi-logout
+        </v-icon>
+
+        Logout
+    </BaseButton>
+</template>
+
+<script setup>
+import BaseButton from '../ui/BaseButton.vue'
+
+const router = useRouter()
+const { logout } = useAuth()
+
+const handleLogout = async () => {
+    await logout()
+    router.push('/')
+}
+</script>
