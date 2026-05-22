eb5f4d496d06c666ce166119cad7a5d9371f67d9
bandilem-git <bandilesprojectidea@gmail.com>
2026-05-22 03:15:04 +0200

    refactor: Refactor ListingGrid to use BaseGrid component
    incorrect file replaced 
    
    Signed-off-by: bandilem-git <bandilesprojectidea@gmail.com>

--- frontend/components/features/marketplace/ListingGrid.vue
+++ frontend/components/features/marketplace/ListingGrid.vue
@@ -1,31 +1,16 @@
-<template> 
-    <div class="grid">
-
-        <ListingCard 
-            v-for="listing in listings"
-            :key="listing.listingId"
-            :listing="listing"
-        />
-    </div>
+<template>
+  <BaseGrid>
+    <ListingCard
+      v-for="listing in listings"
+      :key="listing.listingId"
+      :listing="listing"
+    />
+  </BaseGrid>
 </template>
 
 <script setup>
 import BaseGrid    from '~/components/ui/BaseGrid.vue'
 import ListingCard from './ListingCard.vue'
-defineProps({
-    listings: {
-        type:Array,
-        default: () => []
-    }
-})
-</script>
-
-<style scoped>
-.grid {
-    display: flex;
-
-    grid-template-columns:
-        repeat(auto-fit, minmax(240px, 1fr)); /* auto wraps to the device (desktop and mobile) */
 
 defineProps({
   listings: { type: Array, default: () => [] }
