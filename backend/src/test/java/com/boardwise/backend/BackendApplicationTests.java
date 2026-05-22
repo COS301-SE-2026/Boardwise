d3c0ee2ac83078b32d9f84ae564639af8ae00ad1
W-Wolf-K <175476101+W-Wolf-K@users.noreply.github.com>
2026-05-22 06:38:19 +0200

    chore: removing backend tests

--- backend/src/test/java/com/boardwise/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/boardwise/backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.boardwise.backend;
+// package com.boardwise.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BackendApplicationTests {
+// @SpringBootTest
+// class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
