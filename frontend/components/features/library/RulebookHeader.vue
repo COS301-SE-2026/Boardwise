fdcf9590cf173a6e8f8574cf89292664b138c340
HBPencil28 <u24868346@tuks.co.za>
2026-06-02 02:33:04 +0200

    Removed the tabs on rulebook page as it is incorrectly being used for filters.

--- frontend/components/features/library/RulebookHeader.vue
+++ frontend/components/features/library/RulebookHeader.vue
@@ -2,14 +2,12 @@
     <div class="d-flex flex-column ga-5">
     <SectionTitle title="Library" subtitle="Browse rulebooks and guides" />
     <RulebookSearch @search="$emit('search', $event)" @upload="$emit('upload')" />
-    <RulebookTabs />
   </div>
 </template>
 
 <script setup>
 import SectionTitle from '~/components/ui/SectionTitle.vue'
 import RulebookSearch from './RulebookSearch.vue'
-import RulebookTabs from './RulebookTabs.vue'
 
 defineEmits(['search', 'upload'])
 </script>
