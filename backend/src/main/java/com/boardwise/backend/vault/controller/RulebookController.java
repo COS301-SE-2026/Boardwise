f0407207debaa8e69589187ca9e9d229d5f7c5e6
W-Wolf-K <175476101+W-Wolf-K@users.noreply.github.com>
2026-06-20 07:43:02 +0200

    refactor: Pulled backend from integration

--- backend/src/main/java/com/boardwise/backend/vault/controller/RulebookController.java
+++ backend/src/main/java/com/boardwise/backend/vault/controller/RulebookController.java
@@ -3,7 +3,6 @@ package com.boardwise.backend.vault.controller;
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ import com.boardwise.backend.vault.service.RulebookService;
 
 import lombok.RequiredArgsConstructor;
 
-@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/vault/rulebooks")
 @RequiredArgsConstructor
