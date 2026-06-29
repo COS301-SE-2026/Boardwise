9ad4f1183c040a65f6829b30a61b4cfc60a87429
bandilem-git <u24675394@tuks.co.za>
2026-06-29 12:36:52 +0200

    feat(marketplace): Levenshtein Distance Metric calculation complete Reference: https://en.wikipedia.org/wiki/Levenshtein_distance

--- backend/src/main/java/com/boardwise/backend/scraper/TakealotScraper.java
+++ backend/src/main/java/com/boardwise/backend/scraper/TakealotScraper.java
@@ -1,8 +1,11 @@
 package com.boardwise.backend.scraper;
+import com.boardwise.backend.vault.repository.EditEventRepository;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -15,10 +18,14 @@ import com.microsoft.playwright.Playwright;
 
 @Service
 public class TakealotScraper implements WebScraper {
-    public TakealotScraper(){}
-    
+
+    public TakealotScraper(){} 
+
+    private Path testPdf = Path.of("backend/src/main/java/com/boardwise/backend/scraper/stuff.pdf");
+
     private double stringMatch = 0.4; // >=40% string match gets Returned
     private String searchSelector = "input[placeholder='Search for products, brands...']";
+    private Map<Integer,String> map = new HashMap<>(); 
     private final String site = "https://www.takealot.com";
 
     public List<ScrapeResponse> scrape(String toSearch) {
@@ -40,7 +47,7 @@ public class TakealotScraper implements WebScraper {
             Locator searchBar = page.getByPlaceholder("Search for products, brands...");
 
             if(searchBar.count() == 0){
-                throw new RuntimeException("Error while trying to search Takealot");
+                throw new RuntimeException("Error while trying to find the search bar on Takealot");
             }
 
             //update value found in search bar
@@ -49,9 +56,10 @@ public class TakealotScraper implements WebScraper {
             page.waitForSelector("article[data-ref='product-card']");
 
             //Levenshtein distance - distance between two strings
+            System.out.println(LevenshteinDistance("Bao", "Boo"));
+            // Jaro-Winkler - similarity between 2 sequences
 
-            // Jaro-Winkler - similarity betwen 2 sequences
-            Files.write(Path.of("currPage.pdf"), page.pdf());
+            Files.write(testPdf, page.pdf());
             page.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -59,6 +67,24 @@ public class TakealotScraper implements WebScraper {
 
         return null;
     }
+
+    private float LevenshteinDistance(String a, String b){// STRAIGHT FROM WIKIPEDIA
+
+        if(a.length() == 0) return Math.abs(b.length());// interesting
+        if(b.length() == 0) return Math.abs(a.length());// interesting
+
+        if(a.charAt(0) == b.charAt(0)){
+            return 0 + LevenshteinDistance(a.substring(1),b.substring(1));
+        }
+
+        return 1 + Math.min(LevenshteinDistance(a.substring(1),b),
+        Math.min(LevenshteinDistance(a,b.substring(1)),
+        LevenshteinDistance(a.substring(1),b.substring(1))));
+    }
+
+    // private float Jaro_Wrinkler(String a, String b){
+
+    // }
 
     public static void main(String[] args){
         WebScraper ws = new TakealotScraper();
