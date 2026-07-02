672b9c63277e14e10041d3a59df6f507b551a335
HBPencil28 <u24868346@tuks.co.za>
2026-07-02 15:00:58 +0200

    Events(feature): Components finished for new events page

---
+++ frontend/components/features/events/EventGrid.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-row class="mt-8">
+        <v-col 
+            v-for="event in events"
+            :key="event.id"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+        >
+            <EventCard
+                :event="event"
+                @click="$emit('select', $event)"
+            />
+        </v-col>
+    </v-row>
+</template>
+
+<script setup>
+import EventCard from './EventCard.vue'
+
+defineProps({
+    events: {
+        type: Array,
+        default: () => []
+    }
+})
+
+defineEmits(['select'])
+</script>
