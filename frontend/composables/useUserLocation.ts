016fc0e1a4ee0bee0b1be3d4ee437093397df094
bandilem-git <u24675394@tuks.co.za>
2026-07-06 19:24:09 +0200

    feat(user): getting user location by using geolocation API

---
+++ frontend/composables/useUserLocation.ts
@@ -0,0 +1,64 @@
+    interface NominatimAddress {
+        city?: string;
+        town?: string;
+        village?: string;
+        suburb?: string;
+    }
+
+    interface NominatimResponse {
+        address: NominatimAddress;
+    }
+    
+export async function useUserLocation() {
+    const city = ref<string | null>(null);
+    const suburb = ref<string | null>(null);
+    const lat = ref<number | null>(null);
+    const long = ref<number | null>(null);
+    const error =ref<string | null>(null);
+    const loading = ref(false);
+
+    async function findUserLocation(){
+        loading.value = true;
+
+         try {
+            //raw coordinates
+            const position = await new Promise<GeolocationPosition>((resolve, reject) => {
+                if (!navigator.geolocation) {
+                    reject(new Error('Geolocation not supported by this browser'));
+                    return;
+                }
+                navigator.geolocation.getCurrentPosition(resolve, reject, {
+                    enableHighAccuracy: true,
+                    timeout: 10000,
+                    maximumAge: 0,
+                });
+            });
+
+            lat.value = position.coords.latitude;
+            long.value = position.coords.longitude;
+
+            //find
+            const res = await fetch(
+                `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat.value}&lon=${long.value}`,
+                {
+                    headers: {
+                        'User-Agent': 'Boardwise (worksonmymachine67@gmail.com)'
+                    }
+                }
+            );
+            if (!res.ok) throw new Error('Geocoding request failed');
+
+            const data: NominatimResponse = await res.json();
+            const address = data.address;
+
+            city.value = address.city ?? address.town ?? address.village ?? null;
+            suburb.value = address.suburb ?? null;
+
+        } catch (err) {
+            error.value = err instanceof Error ? err.message : String(err);
+        } finally {
+            loading.value = false;
+        }
+    } 
+    return { city, suburb, lat, long, error, loading, findUserLocation}
+}
