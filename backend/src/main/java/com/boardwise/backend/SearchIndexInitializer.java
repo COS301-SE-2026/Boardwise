02b340a8ccd924f7479601be4b867b0847fa7938
Njabs-codez <njabsmathonsi52@gmail.com>
2026-07-09 17:01:07 +0200

    Chore: Created commandlinerunner for search index creation.

---
+++ backend/src/main/java/com/boardwise/backend/SearchIndexInitializer.java
@@ -0,0 +1,55 @@
+package com.boardwise.backend;
+
+import java.util.List;
+
+import org.bson.Document;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.SearchIndexModel;
+import com.mongodb.client.model.SearchIndexType;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SearchIndexInitializer implements CommandLineRunner{
+
+    private final MongoTemplate template;
+
+    @Override
+    public void run(String... args){
+        MongoCollection<Document> collection = template.getCollection("BOARD_GAME");
+
+        boolean exists = false;
+        for(Document index : collection.listSearchIndexes()){
+            if("boardgame_search".equals(index.getString("name"))){
+                exists = true;
+                break;
+            }
+        }
+
+        if(!exists){
+            Document def = new Document("mappings",
+                new Document("dynamic", false).append(
+                    "fields", new Document("title",
+                        new Document("type", "string").append(
+                            "analyzer", 
+                            "lucene.standard"
+                        )
+                    )
+                )
+            );
+
+            SearchIndexModel model = new SearchIndexModel(
+                "boardgame_search",
+                def,
+                SearchIndexType.search()
+            );
+            collection.createSearchIndexes(List.of(model));
+            System.out.println("Fuzzy Search index created. Name: 'boardgame_search'");
+        }
+    }
+}
