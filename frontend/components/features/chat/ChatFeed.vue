2654264bf413a2d445aaecc9a501923dc219b47c
BridgettNkosi <u22664638@tuks.co.za>
2026-07-16 10:03:37 +0200

    refactor(chat): improve chat feed layout

--- frontend/components/features/chat/ChatFeed.vue
+++ frontend/components/features/chat/ChatFeed.vue
@@ -1,17 +1,26 @@
 <template>
     <BaseCard class="flex-grow-1 overflow-y-auto pa-4">
+        <div v-if="messages.length">
+            <ChatMessage
+                v-for="message in messages"
+                :key="message.id"
+                :message="message"
+            />
+        </div>
         
-        <ChatMessage
-            v-for="message in messages"
-            :key="message.id"
-            :message="message"
+        <BaseEmptyState
+            v-else
+            title="No messages"
+            description="Start the conversation by sending a message."
         />
+
     </BaseCard>
 </template>
 
 <script setup>
 import BaseCard from '~/components/ui/BaseCard.vue';
 import ChatMessage from './ChatMessage.vue';
+import BaseEmptyState from '~/components/ui/BaseEmptyState.vue';
 
 defineProps({
     messages: {
