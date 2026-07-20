f0eb6be36c5b960aa12a9b8003b9a6b55ec32d78
BridgettNkosi <u22664638@tuks.co.za>
2026-07-20 20:35:55 +0200

    chore(landing): scaffold landing page structure

---
+++ frontend/components/features/landing/LandingView.vue
@@ -0,0 +1,21 @@
+<template>
+    <LandingNavbar/>
+
+    <LandingHero/>
+
+    <LandingFeaturedGames/>
+
+    <LandingFeatures/>
+
+    <LandingFooter/>
+</template>
+
+<script setup>
+import LandingFeaturedGames from './LandingFeaturedGames.vue';
+import LandingFeatures from './LandingFeatures.vue';
+import LandingFooter from './LandingFooter.vue';
+import LandingHero from './LandingHero.vue';
+import LandingNavbar from './LandingNavbar.vue';
+
+
+</script>
