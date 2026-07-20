e1445cecf2afb617cf3b419cedc6482e9c01d1f9
BridgettNkosi <u22664638@tuks.co.za>
2026-07-20 20:49:45 +0200

    feat(landing): add landing navigation

---
+++ frontend/components/features/landing/LandingNavbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-app-bar
+        flat
+        color="transparent"
+        class="px-8"
+    >
+
+    <v-toolbar-title class="font-weight-bold">
+        Boardwise
+    </v-toolbar-title>
+
+    <v-spacer/>
+
+    <BaseButton
+        variant="secondary"
+        @click="router.push('/auth/signin')"
+    >
+        Sign In
+    </BaseButton>
+
+    <BaseButton
+        class="ml-3"
+        @click="router.push('/auth/signup')"
+    >
+        Get started
+    </BaseButton>
+    </v-app-bar>
+</template>
+
+<script setup>
+import BaseButton from '~/components/ui/BaseButton.vue';
+
+const router = useRouter()
+</script>
