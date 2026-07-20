2a1baf2a3028d88ee23357811d80fd2a78215cb5
BridgettNkosi <u22664638@tuks.co.za>
2026-07-21 01:51:47 +0200

    feat(landing): scaffold landing page layout

--- frontend/components/features/landing/LandingView.vue
+++ frontend/components/features/landing/LandingView.vue
@@ -3,19 +3,23 @@
 
     <LandingHero/>
 
+    <LandingSetupWizard/>
+
     <LandingFeaturedGames/>
 
     <LandingFeatures/>
+
+    <LandingCallToAction/>
 
     <LandingFooter/>
 </template>
 
 <script setup>
-import LandingFeaturedGames from './LandingFeaturedGames.vue';
-import LandingFeatures from './LandingFeatures.vue';
-import LandingFooter from './LandingFooter.vue';
-import LandingHero from './LandingHero.vue';
-import LandingNavbar from './LandingNavbar.vue';
-
-
+import LandingCallToAction from './LandingCallToAction.vue'
+import LandingFeaturedGames from './LandingFeaturedGames.vue'
+import LandingFeatures from './LandingFeatures.vue'
+import LandingFooter from './LandingFooter.vue'
+import LandingHero from './LandingHero.vue'
+import LandingNavbar from './LandingNavbar.vue'
+import LandingSetupWizard from './LandingSetupWizard.vue'
 </script>
