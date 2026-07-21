b2c821574136cd14f8e17c0ded5220188f3d72a0
Njabs-codez <njabsmathonsi52@gmail.com>
2026-07-21 13:52:41 +0200

    Fix: Addressed Host change on RSVP & deRSVP.

--- backend/src/main/java/com/boardwise/backend/user_service/services/CommunityService.java
+++ backend/src/main/java/com/boardwise/backend/user_service/services/CommunityService.java
@@ -424,7 +424,8 @@ public class CommunityService {
         Example<EventAttendee> example = Example.of(forExample);
         int attendeeCount = ((int) eaRepo.count(example)) + 1;
         
-        EventHostInfo hostInfo = new EventHostInfo(user.getUsername(), user.getProfilePicture());
+        User host = userRepo.findById(event.getCreatorId()).get();
+        EventHostInfo hostInfo = new EventHostInfo(host.getUsername(), host.getProfilePicture());
 
         List<Boardgame> dbGames = gameRepo.findAllById(event.getGames());
         List<GameInventoryDTO> games = new ArrayList<>();
@@ -470,7 +471,8 @@ public class CommunityService {
         Example<EventAttendee> example = Example.of(forExample);
         int attendeeCount = ((int) eaRepo.count(example)) + 1;
         
-        EventHostInfo hostInfo = new EventHostInfo(user.getUsername(), user.getProfilePicture());
+        User host = userRepo.findById(event.getCreatorId()).get();
+        EventHostInfo hostInfo = new EventHostInfo(host.getUsername(), host.getProfilePicture());
 
         List<Boardgame> dbGames = gameRepo.findAllById(event.getGames());
         List<GameInventoryDTO> games = new ArrayList<>();
