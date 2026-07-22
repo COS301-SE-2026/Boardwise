af7f76e89210eb89e064bc8a5d88ed0c760ca157
BridgettNkosi <u22664638@tuks.co.za>
2026-07-22 22:57:14 +0200

    refactor: redesign onboarding cards

--- frontend/components/features/landing/LandingGameCard.vue
+++ frontend/components/features/landing/LandingGameCard.vue
@@ -1,37 +1,38 @@
 <template>
-    <BaseCard class="pa-6 h-100">
+    <BaseCard class="pa-6 text-center h-100 cursor-pointer d-flex flex-column justify-center"
+    @click="navigateTo(game)">
+
         <v-icon
-            :src="game.imageUrl"
-            height="220"
-            contain
-            class="rounded-lg mb-4"
+            :icon="game.icon"
+            size="64"
+            color="secondary"
+            class="mb-4"
         />
 
-        <v-chip
-            size="small"
-            color="primary"
-            class="mb-3"
-        >
-            {{ game.genres?.[0] || 'Board game' }}
-        </v-chip>
-
-        <h3 class="text-h5 font-weight-bold mb-3">
+        <h3>
             {{ game.title }}
         </h3>
 
-        <p class="text-body-2 text-medium-emphasis mt-2">
+        <h3>
             {{ game.description }}
-        </p>
+        </h3>
+
     </BaseCard>
 </template>
 
 <script setup>
 import BaseCard from '~/components/ui/BaseCard.vue'
 
-defineProps({
+const router = useRouter()
+
+const props = defineProps({
     game: {
         type: Object,
         required: true
     }
 })
+
+const navigateTo = (game) => {
+    router.push(game.route)
+}
 </script>
