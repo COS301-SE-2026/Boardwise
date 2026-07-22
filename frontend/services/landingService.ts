f2cc0f9c9a4241d0e46aee93917197dfa936d6e1
BridgettNkosi <u22664638@tuks.co.za>
2026-07-22 22:57:26 +0200

    refactor: simplify landing service

--- frontend/services/landingService.ts
+++ frontend/services/landingService.ts
@@ -1,9 +1,14 @@
-import { featuredGames, platformFeatures } from './mockData/landing'
+import { onboardingSteps, platformFeatures } from './mockData/landing'
+import { games } from './mockData/games'
 
-export const getFeaturedGames = () => {
-    return [...featuredGames]
+export const getOnBoardingSteps = () => {
+    return [...onboardingSteps]
 }
 
 export const getPlatformFeatures = () => {
     return [...platformFeatures]
+}
+
+export const getLandingGames = () => {
+    return [...games]
 }
