e60a5cf3f72c174c2b758759064331da4baea7da
BridgettNkosi <u22664638@tuks.co.za>
2026-07-23 05:07:28 +0200

    feat: add help view layout

---
+++ frontend/components/features/help/HelpView.vue
@@ -0,0 +1,16 @@
+<template>
+    <PageContainer>
+        <Navbar/>
+        <HelpHero />
+        <HelpFAQ />
+        <HelpContact />
+    </PageContainer>
+</template>
+
+<script setup>
+import PageContainer from '~/components/layout/PageContainer.vue'
+import HelpHero from './HelpHero.vue'
+import HelpFAQ from './HelpFAQ.vue'
+import HelpContact from './HelpContact.vue'
+import Navbar from '~/components/layout/Navbar.vue';
+</script>
