3c3c98e29c1dcdd50649a9960ae49eaf33316bb5
HBPencil28 <u24868346@tuks.co.za>
2026-07-23 04:31:48 +0200

    Design(task): Style Guide Typography  component for Brand Style Guide.

--- frontend/pages/brandStyleGuide/index.vue
+++ frontend/pages/brandStyleGuide/index.vue
@@ -6,6 +6,7 @@
             <v-container class="max-width-container">
                 <StyleHero />
                 <StyleColours />
+                <StyleTypography />
             </v-container>
 
             <footer class="py-8 section-border text-center">
@@ -21,8 +22,9 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import StyleColours from '~/components/brandStyleGuide/StyleColours.vue';
 
+import StyleTypography from '~/components/brandStyleGuide/StyleTypography.vue';
+import StyleColours from '~/components/brandStyleGuide/StyleColours.vue';
 import StyleHero from '~/components/brandStyleGuide/StyleHero.vue';
 import StyleNav from '~/components/brandStyleGuide/StyleNav.vue';
 
