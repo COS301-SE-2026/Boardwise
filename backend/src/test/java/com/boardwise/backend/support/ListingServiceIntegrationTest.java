cc799189d84df397d2ec661ffc04f536f1cfc395
bandilem-git <u24675394@tuks.co.za>
2026-08-20 22:12:21 +0200

    feat(marketplace): integration tests

---
+++ backend/src/test/java/com/boardwise/backend/support/ListingServiceIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.boardwise.backend.support;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.*;
+
+import com.boardwise.backend.marketplace.dtos.listing.ListingResponse;
+import com.boardwise.backend.marketplace.enums.Condition;
+import com.boardwise.backend.marketplace.enums.Genres;
+import com.boardwise.backend.marketplace.enums.ItemType;
+import com.boardwise.backend.marketplace.enums.ListingStatus;
+import com.boardwise.backend.marketplace.model.Listing;
+import com.boardwise.backend.marketplace.repository.ListingRepository;
+import com.boardwise.backend.marketplace.service.ListingService;
+
+public class ListingServiceIntegrationTest extends AbstractIntegrationTest {
+
+    @BeforeEach
+    void setUp() {
+        listingRepository.deleteAll();
+    }
+
+    @Autowired
+    private ListingService listingService;
+
+    @Autowired
+    private ListingRepository listingRepository;
+
+    @Test
+    void shouldStoreAndRetrieveListing(){
+        Listing listing = new Listing("rtickyv", "IamR3al", new ObjectId(), ItemType.FullBoardGame.name(), "sale", 50.0, "Hatfied", true, "title", Condition.FAIR.name(), "Monopoly", "gg", "desc", "image", ListingStatus.AVAILABLE, LocalDateTime.now(), LocalDateTime.now(), List.of(Genres.ABSTRACT.getValue()), null);
+        listingRepository.save(listing);
+        List<ListingResponse> results = listingService.getAllActiveListings();
+
+        assertThat(results).hasSize(1);
+    }
+
+    
+}
