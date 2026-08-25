6ba91eccf39d022ef5d47aa4f408e8e679e8b9b0
HBPencil28 <u24868346@tuks.co.za>
2026-08-25 09:30:32 +0200

    feat(globalSearch): Added the PersonCard component for future friend component and the SectionHeader for globalSearch

---
+++ frontend/components/features/people/PersonCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <BaseCard class="cursor-pointer" @click="$emit('click', person)">
+        <BaseImage 
+            :src="person.avatarUrl ?? '/default-avatar.png'"
+            :alt="person.username"
+            height="180px"
+        />
+
+        <div class="pa-4 d-flex flex-column ga-2">
+            <h3 class="card-title">@{{  person.username }}</h3>
+
+            <p class="card-meta">
+                {{  person.mutuaLabel }}
+            </p>
+
+            <BaseButton
+                class="mt-auto"
+                block
+                :variant="person.isFriend? 'secondary' : 'primary'"
+                @click.stop="$emit('friend-action', person)"
+            >
+                {{  person.isFriend ? 'Friends':  'Add Friend'}}
+            </BaseButton>
+        </div>
+    </BaseCard>
+</template>
+
+<script setup>
+import BaseButton from '~/components/ui/BaseButton.vue';
+import BaseImage from '~/components/ui/BaseImage.vue';
+import BaseCard from '~/components/ui/BaseCard.vue';
+
+defineProps({
+    person: {
+        type: Object,
+        required: true
+    }
+})
+
+defineEmits(['click', 'friend-action'])
+</script>
