639ea47bb0c43a6f8586488d3f241227be75a28c
HBPencil28 <u24868346@tuks.co.za>
2026-08-26 18:35:48 +0200

    feat(friends): Friend Action Button component.

---
+++ frontend/components/features/people/FriendActionButton.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { friendService, type FriendStatus } from '~/services/friendService'
+
+const props = defineProps<{
+    status: FriendStatus
+}>()
+
+const emit = defineEmits<{
+    (e: 'add'): void
+    (e: 'remove'): void
+}>()
+
+const label = computed(() => {
+    switch(props.status) {
+        case 'friends': return 'Friends'
+        case 'pendingSent' : return 'Request Sent'
+        case 'pendingReceived': return 'Respond'
+        default: return 'Add Friend'
+    }
+})
+
+const variant = computed(() => props.status === 'none' ? 'primary': 'secondary')
+
+const handleClick = () => {
+    if(props.status === 'friends') emit('remove')
+    else if (props.status === 'none') emit('add')
+
+    //pendingReceived 
+}
+</script>
