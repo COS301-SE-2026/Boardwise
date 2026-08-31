8d548e438e8ade639a64d6ba93345e4c0486c825
BridgettNkosi <u22664638@tuks.co.za>
2026-08-31 23:09:20 +0200

    fix(a11y): improve textarea labelling

--- frontend/components/ui/BaseTextArea.vue
+++ frontend/components/ui/BaseTextArea.vue
@@ -1,19 +1,34 @@
 <template>
   <v-textarea
+    v-model="model"
+    :label="label"
     :placeholder="placeholder"
     :rows="rows"
-    v-model="model"
+    :aria-label="label || ariaLabel"
     variant="outlined"
     rounded="lg"
     auto-grow
+    hide-details="auto"
+    v-bind="$attrs"
   />
 </template>
 
 <script setup>
+defineOptions({
+  inheritAttrs: false
+})
 defineProps({
   placeholder: {
     type: String,
     default: ''
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  ariaLabel: {
+    type: Sring,
+    default: 'Text input'
   },
   rows: {
     type: Number,
