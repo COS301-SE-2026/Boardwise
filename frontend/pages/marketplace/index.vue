523e1c11c6ec312fe4dd97a9fe85e8e7dc20d5d8
HBPencil28 <u24868346@tuks.co.za>
2026-09-04 02:59:37 +0200

    Marketplace(fix): Fixed filters on mobile.

--- frontend/pages/marketplace/index.vue
+++ frontend/pages/marketplace/index.vue
@@ -32,7 +32,10 @@
 
       <!-- Desktop -->
       <div class="d-flex d-md-flex ga-6 mt-6 align-start">
-        <FilterSidebar data-test="filter-sidebar" @filter="handleFilter"/>
+        <div class="d-none d-md-block">
+          <FilterSidebar data-test="filter-sidebar" @filter="handleFilter"/>
+        </div>
+          
         <v-container v-if="loading" class="d-flex justify-center align-center" style="min-height: 60vh">
           <v-progress-circular data-test="loading-spinner" indeterminate color="primary" size="48" />
         </v-container>
