28767421a4e0b177a0a0a0f0245cd1d7c3edefa3
HBPencil28 <u24868346@tuks.co.za>
2026-09-07 16:09:55 +0200

    Layout(fix): Fixed spacing between section title and search bar

--- frontend/components/layout/PageHeader.vue
+++ frontend/components/layout/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex justify-space-between align-center flex-wrap ga-3 mb-6">
+  <div class="d-flex justify-space-between align-center flex-wrap ga-3 mb-3">
 
     <div>
       <h1 class="text-h4 font-weight-bold">{{ title }}</h1>
