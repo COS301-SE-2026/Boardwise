ea63d27206d52d2304eaafa85a444429b8852c1e
HBPencil28 <u24868346@tuks.co.za>
2026-09-11 00:24:47 +0200

    Marketplace(task): Empty state component fixed..

--- frontend/components/features/marketplace/MarketplaceEmptyState.vue
+++ frontend/components/features/marketplace/MarketplaceEmptyState.vue
@@ -34,7 +34,7 @@
 </template>
 
 <script setup>
-import { compile, computed } from 'vue'
+import { computed } from 'vue'
 
 import BaseImage from '~/components/ui/BaseImage.vue';
 import BaseButton from '~/components/ui/BaseButton.vue';
