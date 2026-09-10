547f5d54792286bbf45a34a1df3efb986cd2304a
HBPencil28 <u24868346@tuks.co.za>
2026-09-10 11:18:29 +0200

    Marketplace(task): Updated the search and header to match the wireframes better

--- frontend/components/features/marketplace/MarketplaceHeader.vue
+++ frontend/components/features/marketplace/MarketplaceHeader.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="d-flex flex-column ga-5" data-test="marketplace-header">
-
+    
     <SectionTitle
       data-test="marketplace-title"
       title="Marketplace"
       subtitle="Buy, Rent and List board games with the community"
     />
+
+    <div class="marketplace-header__actions">
+      <BaseButton 
+        data-test="create-listing-button" 
+        variant="primary" 
+        prepend-icon="mdi-plus" 
+        @click="$emit('create-listing')"
+      > 
+          Create Listing
+      </BaseButton>
+    </div>
 
     <MarketplaceSearch
       data-test="marketplace-search"
@@ -18,6 +29,14 @@
 <script setup>
 import SectionTitle from '~/components/ui/SectionTitle.vue'
 import MarketplaceSearch from './MarketplaceSearch.vue';
+import BaseButton from '~/components/ui/BaseButton.vue';
 
 defineEmits(['search', 'create-listing'])
 </script>
+
+<style scoped>
+.marketplace-header__actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
