a726e0c8466c1fa76fa73eff489d85b8201e8057
bandilem-git <u24675394@tuks.co.za>
2026-09-12 21:03:01 +0200

    chore(webscrapers): registered webscrapers as their own service

---
+++ webscrapers/src/main/java/bosrdwise/scrapers/webscrapers/WebscrapersApplication.java
@@ -0,0 +1,13 @@
+package bosrdwise.scrapers.webscrapers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebscrapersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebscrapersApplication.class, args);
+	}
+
+}
---
+++ webscrapers/src/test/java/bosrdwise/scrapers/webscrapers/WebscrapersApplicationTests.java
@@ -0,0 +1,13 @@
+package bosrdwise.scrapers.webscrapers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscrapersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
