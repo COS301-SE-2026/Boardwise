f6807375613c970ca369d8c99bd4c616821f5fdf
bandilem-git <u24675394@tuks.co.za>
2026-09-19 00:59:39 +0200

    task(UI): Base Image audit

--- frontend/components/ui/BaseAvatar.vue
+++ frontend/components/ui/BaseAvatar.vue
@@ -1,12 +1,12 @@
 <template>
   <v-avatar :size="sizeMap[size] || sizeMap.md" clas="base-avatar">
-    <v-img 
+    <BaseImage 
       v-if="src && !imageError"
       :src="src"
       :alt="resolvedAlt"
       cover
       @error="imageError = true"
-    ></v-img>
+    ></BaseImage>
 
     <span
       v-else
@@ -20,6 +20,7 @@
 
 <script setup>
 import { computed, ref } from 'vue'
+import BaseImage from '~/components/ui/BaseImage.vue'
 
 const props = defineProps({
   src: {
