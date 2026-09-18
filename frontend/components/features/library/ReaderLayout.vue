a3932f6587960dfe0172d4deb8983c9f147e3a50
HBPencil28 <u24868346@tuks.co.za>
2026-09-19 00:40:19 +0200

    Library(task): Updated the mobile version for reading rulebooks.

--- frontend/components/features/library/ReaderLayout.vue
+++ frontend/components/features/library/ReaderLayout.vue
@@ -30,8 +30,34 @@
       <ReaderProgress :current-page="activeChunkIndex" :total-pages="localChunks.length" />
     </div>
 
+    <!-- Mobile contents trigger -->
+     <div class="d-flex d-md-none mt-4 mb-2 px-3">
+      <v-chip
+        color="secondary"
+        prepend-icon="mdi-format-list-bulleted"
+        size="large"
+        @click="showContents = true"
+      >
+        Contents
+      </v-chip>
+
+      <v-navigation-drawer
+        v-model="showContents"
+        temporary
+        location="left"
+        width="300"
+      >
+        <ReaderSidebar
+          :pages="localChunks"
+          :current-page="activeChunkIndex"
+          :matching-chunks="matchingChunkIndices"
+          @change="(index) => { handlePageChange(index); showContents = false }"
+        />
+      </v-navigation-drawer>
+     </div>
+
     <v-row>
-      <v-col cols="12" md="3">
+      <v-col cols="12" md="3" class="d-none d-md-block">
         <ReaderSidebar
           :pages="localChunks"
           :current-page="activeChunkIndex"
@@ -45,7 +71,7 @@
           <!-- Document Header -->
           <BaseImage :src="rulebook?.coverUrl" :alt="rulebook?.title" height="280px" fit="cover" />
 
-          <div class="pa-10 pt-10 pb-2">
+          <div class="pa-4 pa-md-10 pt-4 pt-md-10 pb-2">
             <p class="text-caption text-uppercase font-weight-bold text-primary mb-2">
               {{ formattedGenres }}
             </p>
@@ -136,6 +162,7 @@ const blockRefs = ref({})
 const searchQuery = ref('')
 const currentMatch = ref(0)
 const showRagPanel = ref(false)
+const showContents = ref(false)
 
 const setBlockRef = (el, chunkId) => {
   if(el){
--- frontend/components/features/library/ReaderToolbar.vue
+++ frontend/components/features/library/ReaderToolbar.vue
@@ -1,162 +1,240 @@
 <template>
-  <v-app-bar flat border="b" color="surface" height="64" style="position: sticky; top: 0; z-index: 20;">
+  <div>
+    <v-app-bar flat border="b" color="surface" height="64" style="position: sticky; top: 0; z-index: 20;">
 
-    <NuxtLink :to="`/library`" class="text-primary font-weight-bold text-decoration-none ml-4">
-      <v-icon start>mdi-arrow-left</v-icon>
-      Back
-    </NuxtLink>
+      <NuxtLink :to="`/library`" class="text-primary font-weight-bold text-decoration-none ml-4 d-flex align-center">
+        <v-icon start>mdi-arrow-left</v-icon>
+        <span class="d-none d-sm-inline">Back</span>
+      </NuxtLink>
 
-    <v-app-bar-title class="text-body-1 font-weight-medium">
-      {{ rulebook?.title }}
-    </v-app-bar-title>
+      <v-app-bar-title class="text-body-1 font-weight-medium text-truncate">
+        {{ rulebook?.title }}
+      </v-app-bar-title>
 
-    <template #append>
-      <div class="d-flex align-center ga-2 mr-4">
+      <template #append>
+        <div class="d-flex align-center ga-2 mr-4">
 
-        <!-- Lock indicator + countdown -->
-         <template v-if="lockHeldBy || isEditing">
-          <span 
-            class="text-caption text-no-wrap d-flex align-center ga-1"
-            :class="isEditing ? 'text-success' : 'text-error'"
-          >
-            <v-icon size="14" :color="isEditing ? 'success' : 'error'"> mdi-lock</v-icon>
-            {{ isEditing? 'You are editing' : `@${lockHeldBy}` }}
-            <template v-if="countdown > 0">
-              {{  formattedCountdown }}
-            </template>
-          </span>
-         </template>
+          <!-- Lock indicator + countdown -->
+           <template v-if="lockHeldBy || isEditing">
+            <span 
+              class="text-caption text-no-wrap d-none d-sm-flex align-center ga-1"
+              :class="isEditing ? 'text-success' : 'text-error'"
+            >
+              <v-icon size="14" :color="isEditing ? 'success' : 'error'"> mdi-lock</v-icon>
+              {{ isEditing? 'You are editing' : `@${lockHeldBy}` }}
+              <template v-if="countdown > 0">
+                {{  formattedCountdown }}
+              </template>
+            </span>
+           </template>
 
-         <!-- Undo/Redo - only while editing -->
-          <template v-if="isEditing">
-              <v-tooltip text="Undo" location="bottom">
-                <template #activator="{ props: tooltipProps }">
-                  <BaseButton
-                    v-bind="tooltipProps"
-                    icon size="small" variant="text"
-                    :disabled="!canUndo"
-                    @click="emit('undo')"
-                  >
-                    <v-icon>mdi-undo</v-icon>
-                  </BaseButton>
-                </template>
-              </v-tooltip>
+           <!-- Desktop: Undo/Redo inline. Mobile: tucked into the overflow menu below -->
+            <template v-if="isEditing && mdAndUp">
+                <v-tooltip text="Undo" location="bottom">
+                  <template #activator="{ props: tooltipProps }">
+                    <BaseButton
+                      v-bind="tooltipProps"
+                      icon size="small" variant="text"
+                      :disabled="!canUndo"
+                      @click="emit('undo')"
+                    >
+                      <v-icon>mdi-undo</v-icon>
+                    </BaseButton>
+                  </template>
+                </v-tooltip>
 
-              <v-tooltip text="Redo" location="bottom">
-                <template #activator="{ props: tooltipProps }">
-                  <BaseButton
-                    v-bind="tooltipProps"
-                    icon size="small" 
-                    variant="text"
-                    :disabled="!canRedo"
-                    @click="emit('redo')"
-                  >
-                    <v-icon>mdi-redo</v-icon>
-                  </BaseButton>
-                </template>
-              </v-tooltip>
-          </template>
-
-            <!-- Edit/ Done Editing Button -->
-            <template v-if="isEditing">
-              <BaseButton
-                size="sm"
-                variant="secondary"
-                prepend-icon="mdi-check"
-                @click="emit('stop-editing')"
-              >
-                Done Editing
-              </BaseButton>
-            </template>
-            <template v-else>
-              <v-tooltip
-                :text="lockHeldBy ? `Currently being edited by @${lockHeldBy}` : 'Edit this section'"
-                location="bottom"
-              >
-                <template #activator="{ props: tooltipProps }">
-                  <BaseButton
-                    v-bind="tooltipProps"
-                    size="sm"
-                    variant="secondary"
-                    prepend-icon="mdi-pencil"
-                    :disabled="!!lockHeldBy"
-                    @click="emit('edit')"
-                  >
-                    Edit
-                  </BaseButton>
-                </template>
-              </v-tooltip>
+                <v-tooltip text="Redo" location="bottom">
+                  <template #activator="{ props: tooltipProps }">
+                    <BaseButton
+                      v-bind="tooltipProps"
+                      icon size="small" 
+                      variant="text"
+                      :disabled="!canRedo"
+                      @click="emit('redo')"
+                    >
+                      <v-icon>mdi-redo</v-icon>
+                    </BaseButton>
+                  </template>
+                </v-tooltip>
             </template>
 
-            <!-- Download Button -->
-            <v-tooltip text="Download PDF" location="bottom">
-              <template #activator="{ props: tooltipProps }">
+              <!-- Edit/ Done Editing Button -->
+              <template v-if="isEditing">
                 <BaseButton
-                  v-bind="tooltipProps"
-                  icon size="sm"
-                  variant="text"
-                  :loading="isDownloading"
-                  :disabled="!rulebook?.id"
-                  @click="handleDownload"
+                  size="sm"
+                  variant="secondary"
+                  :prepend-icon="mdAndUp ? 'mdi-check' : undefined"
+                  :icon="!mdAndUp ? true : undefined"
+                  @click="emit('stop-editing')"
                 >
-                  <v-icon>mdi-download</v-icon>
+                  <v-icon v-if="!mdAndUp">mdi-check</v-icon>
+                  <span v-if="mdAndUp">Done Editing</span>
                 </BaseButton>
               </template>
-            </v-tooltip>
+              <template v-else>
+                <v-tooltip
+                  :text="lockHeldBy ? `Currently being edited by @${lockHeldBy}` : 'Edit this section'"
+                  location="bottom"
+                >
+                  <template #activator="{ props: tooltipProps }">
+                    <BaseButton
+                      v-bind="tooltipProps"
+                      size="sm"
+                      variant="secondary"
+                      :prepend-icon="mdAndUp ? 'mdi-pencil' : undefined"
+                      :icon="!mdAndUp ? true : undefined"
+                      :disabled="!!lockHeldBy"
+                      @click="emit('edit')"
+                    >
+                      <v-icon v-if="!mdAndUp">mdi-pencil</v-icon>
+                      <span v-if="mdAndUp">Edit</span>
+                    </BaseButton>
+                  </template>
+                </v-tooltip>
+              </template>
 
-            <!-- History Button -->
-            <BaseButton 
-              icon size="small" 
-              variant="text"
-              @click="emit('toggle-history')"
-            >
-              <v-icon>mdi-history</v-icon>
-            </BaseButton>
+              <!-- Desktop: Download + History inline. Mobile: overflow menu -->
+              <template v-if="mdAndUp">
+                <v-tooltip text="Download PDF" location="bottom">
+                  <template #activator="{ props: tooltipProps }">
+                    <BaseButton
+                      v-bind="tooltipProps"
+                      icon size="sm"
+                      variant="text"
+                      :loading="isDownloading"
+                      :disabled="!rulebook?.id"
+                      @click="handleDownload"
+                    >
+                      <v-icon>mdi-download</v-icon>
+                    </BaseButton>
+                  </template>
+                </v-tooltip>
 
-            <!-- Search  -->
-            <template v-if="showSearch">
-              <BaseSearch 
-                v-model="localQuery"
-                placeholder="Search in this rulebook"
-                autofocus
-                style="width: 220px;"
-                @update:model-value="emit('search', $event)"
-                @keydown.enter="$emit('next-match')"
-                @keydown.escape="closeSearch"
-              />
+                <BaseButton 
+                  icon size="small" 
+                  variant="text"
+                  @click="emit('toggle-history')"
+                >
+                  <v-icon>mdi-history</v-icon>
+                </BaseButton>
+              </template>
 
-              <span v-if="localQuery" class="text-caption text-medium-emphasis text-no-wrap">
-                {{ matchCount > 0 ? `${currentMatch + 1} / ${matchCount}`: 'No matches' }}
-              </span>
+              <!-- Mobile overflow menu: Undo/Redo/Download/History -->
+              <v-menu v-if="!mdAndUp" location="bottom end">
+                <template #activator="{ props: menuProps }">
+                  <BaseButton icon size="small" variant="text" v-bind="menuProps" aria-label="More actions">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </BaseButton>
+                </template>
 
-              <BaseButton icon size="small" variant="text" :disabled="matchCount === 0" @click="$emit('prev-match')">
-                <v-icon size="18">mdi-chevron-up</v-icon>
+                <v-list density="compact" min-width="180">
+                  <v-list-item
+                    v-if="isEditing"
+                    prepend-icon="mdi-undo"
+                    title="Undo"
+                    :disabled="!canUndo"
+                    @click="emit('undo')"
+                  />
+                  <v-list-item
+                    v-if="isEditing"
+                    prepend-icon="mdi-redo"
+                    title="Redo"
+                    :disabled="!canRedo"
+                    @click="emit('redo')"
+                  />
+                  <v-list-item
+                    prepend-icon="mdi-download"
+                    title="Download PDF"
+                    :disabled="!rulebook?.id || isDownloading"
+                    @click="handleDownload"
+                  />
+                  <v-list-item
+                    prepend-icon="mdi-history"
+                    title="History"
+                    @click="emit('toggle-history')"
+                  />
+                </v-list>
+              </v-menu>
+
+              <!-- Search (desktop: inline) -->
+              <template v-if="mdAndUp">
+                <template v-if="showSearch">
+                  <BaseSearch 
+                    v-model="localQuery"
+                    placeholder="Search in this rulebook"
+                    autofocus
+                    style="width: 220px;"
+                    @update:model-value="emit('search', $event)"
+                    @keydown.enter="$emit('next-match')"
+                    @keydown.escape="closeSearch"
+                  />
+
+                  <span v-if="localQuery" class="text-caption text-medium-emphasis text-no-wrap">
+                    {{ matchCount > 0 ? `${currentMatch + 1} / ${matchCount}`: 'No matches' }}
+                  </span>
+
+                  <BaseButton icon size="small" variant="text" :disabled="matchCount === 0" @click="$emit('prev-match')">
+                    <v-icon size="18">mdi-chevron-up</v-icon>
+                  </BaseButton>
+
+                  <BaseButton icon size="small" variant="text" :disabled="matchCount === 0" @click="$emit('next-match')">
+                    <v-icon size="18">mdi-chevron-down</v-icon>
+                  </BaseButton>
+
+                  <BaseButton icon size="small" variant="text" @click="closeSearch">
+                    <v-icon size="18">mdi-close</v-icon>
+                  </BaseButton>
+
+                  <span class="text-caption text-medium-emphasis text-no-wrap">
+                    {{ currentPage + 1 }} / {{ totalPages }}
+                  </span>
+                </template>
+                <template v-else>
+                  <BaseButton icon size="small" variant="text" @click="showSearch = !showSearch">
+                    <v-icon>mdi-magnify</v-icon>
+                  </BaseButton>
+                </template>
+              </template>
+
+              <!-- Search toggle (mobile: opens the row below instead of inline) -->
+              <BaseButton v-else icon size="small" variant="text" @click="showSearch = !showSearch">
+                <v-icon>{{ showSearch ? 'mdi-close' : 'mdi-magnify' }}</v-icon>
               </BaseButton>
+        </div>
+      </template>
+    </v-app-bar>
 
-              <BaseButton icon size="small" variant="text" :disabled="matchCount === 0" @click="$emit('next-match')">
-                <v-icon size="18">mdi-chevron-down</v-icon>
-              </BaseButton>
+    <!-- Mobile search row: its own full-width bar below the toolbar,
+         instead of trying to cram into the fixed-height app-bar -->
+    <div v-if="!mdAndUp && showSearch" class="mobile-search-row d-flex align-center ga-2 pa-2">
+      <BaseSearch 
+        v-model="localQuery"
+        placeholder="Search in this rulebook"
+        autofocus
+        class="flex-grow-1"
+        @update:model-value="emit('search', $event)"
+        @keydown.enter="$emit('next-match')"
+        @keydown.escape="closeSearch"
+      />
 
-              <BaseButton icon size="small" variant="text" @click="closeSearch">
-                <v-icon size="18">mdi-close</v-icon>
-              </BaseButton>
+      <span v-if="localQuery" class="text-caption text-medium-emphasis text-no-wrap">
+        {{ matchCount > 0 ? `${currentMatch + 1} / ${matchCount}`: 'No matches' }}
+      </span>
 
-              <span class="text-caption text-medium-emphasis text-no-wrap">
-                {{ currentPage + 1 }} / {{ totalPages }}
-              </span>
-            </template>
-            <template v-else>
-              <BaseButton icon size="small" variant="text" @click="showSearch = !showSearch">
-                <v-icon>mdi-magnify</v-icon>
-              </BaseButton>
-            </template>
-      </div>
-    </template>
-  </v-app-bar>
+      <BaseButton icon size="small" variant="text" :disabled="matchCount === 0" @click="$emit('prev-match')">
+        <v-icon size="18">mdi-chevron-up</v-icon>
+      </BaseButton>
+
+      <BaseButton icon size="small" variant="text" :disabled="matchCount === 0" @click="$emit('next-match')">
+        <v-icon size="18">mdi-chevron-down</v-icon>
+      </BaseButton>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { ref, computed, onUnmounted ,watch } from 'vue'
+import { useDisplay } from 'vuetify'
 import BaseSearch from '~/components/ui/BaseSearch.vue'
 import { useSnackBar } from '~/composables/useSnackbar.ts'
 import { useLibrary } from '~/composables/useLibrary'
@@ -164,6 +242,7 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute();
 const { show } = useSnackBar();
+const { mdAndUp } = useDisplay();
 
 const { getDownloadLink, downloadUrl } = useLibrary();
 
