9a61c1469b86333bd25baaf094b41b7eb51adc60
bandilem-git <u24675394@tuks.co.za>
2026-09-20 00:54:41 +0200

    fix(marketplace): add and remove

--- frontend/components/features/profile/AddListingCard.vue
+++ frontend/components/features/profile/AddListingCard.vue
@@ -3,19 +3,12 @@
     label="Add Listing"
     @click="showAdd = true"
   />
-
-  <AddListingModal 
-    v-model="showAdd"
-    @confirm="handleConfirm"
-  />
 </template>
 
 <script setup>
 import BaseAddCard from '~/components/ui/BaseAddCard.vue'
-import AddListingModal from '~/components/features/profile/AddListingModal.vue'
 
 import { ref } from 'vue'
 
-const { addListing } = useMarketplace()
 const showAdd = ref(false)
 </script>
