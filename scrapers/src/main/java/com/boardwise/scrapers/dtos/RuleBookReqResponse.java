9a3184ad89133151ebd13957ff748005a6fbfb6f
bandilem-git <u24675394@tuks.co.za>
2026-09-21 03:06:15 +0200

    feat(scrfapers): scraper controller for ondemand scraping of rulebooks

--- scrapers/src/main/java/com/boardwise/scrapers/dtos/RuleBookReqResponse.java
+++ scrapers/src/main/java/com/boardwise/scrapers/dtos/RuleBookReqResponse.java
@@ -3,8 +3,11 @@ package com.boardwise.scrapers.dtos;
 
 import org.bson.types.ObjectId;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
 import jakarta.validation.constraints.NotNull;
 
 public record RuleBookReqResponse(
-    @NotNull ObjectId id
+       @NotNull @JsonSerialize(using = ToStringSerializer.class) ObjectId id
 ) {}
