4d48e05384b13f97eb62f3a5b45f9528d34ab372
HBPencil28 <u24868346@tuks.co.za>
2026-09-25 03:34:03 +0200

    Friends(feat): Implemented PlayerGrid component for friends page.

---
+++ frontend/components/features/friends/PlayerGrid.vue
@@ -0,0 +1,29 @@
+<template>
+    <BaseGrid data-test="player-grid">
+        <PlayerCard
+            v-for="player in players"
+            :key="player.id"
+            :player="player"
+            :variant="variant"
+            @friend-request="$emit('friend-request', $event)"
+            @message="$emit('message', $event)"
+            @unfriend="$emit('unfriend', $event)"
+        />
+    </BaseGrid>
+</template>
+
+<script setup>
+import PlayerCard from './PlayerCard.vue'
+import BaseGrid from '~/components/ui/BaseGrid.vue'
+
+defineProps({
+    players: { type: Array, default: () => []},
+    variant: {
+        type: String,
+        default: 'discover',
+        validaor: (v) => ['discover', 'friend'].includes(v)
+    }
+})
+
+defineEmits(['friend-request', 'message', 'unfriend'])
+</script>
